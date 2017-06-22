840b202bacfc2912b629f83b347dbb5d74c0c8e2
BrightLight <ml@silverseed.de>
2017-06-22 20:54:20 +0200

    Get full stack trace of errors when maven runs.

--- appveyor.ps1
+++ appveyor.ps1
@@ -131,7 +131,7 @@ switch ($env:RUN_ITS)
 		echo "InstallAppveyorTools complete."
 
 		echo "Running mvn package"
-		mvn package "--batch-mode" "-Dsource.skip=true" "-Denforcer.skip=true" "-Danimal.sniffer.skip=true" "-Dmaven.test.skip=true"
+		mvn package "--batch-mode" "-Dsource.skip=true" "-Denforcer.skip=true" "-Danimal.sniffer.skip=true" "-Dmaven.test.skip=true" "--errors"
 		CheckLastExitCode
 
 		if ($env:SQ_VERSION -eq "DEV")
