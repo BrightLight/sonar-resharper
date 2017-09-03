ab1fea5f6cea5bb22dc808ae38cbd8fc66f14fc9
BrightLight <ml@silverseed.de>
2017-09-03 16:49:07 +0200

    I hate maven and SonarQube...

--- appveyor.ps1
+++ appveyor.ps1
@@ -131,7 +131,7 @@ switch ($env:RUN_ITS)
 		echo "InstallAppveyorTools complete."
 
 		echo "Running mvn package"
-		mvn package "--batch-mode" "-Dlicense=skip" "-Dsource.skip=true" "-Denforcer.skip=true" "-Danimal.sniffer.skip=true" "-Dmaven.test.skip=true" "--errors" "-Dmaven.compiler.showDeprecation=true"
+		mvn package "--batch-mode" "-Dlicense.skip=true" "-Dsource.skip=true" "-Denforcer.skip=true" "-Danimal.sniffer.skip=true" "-Dmaven.test.skip=true" "--errors" "-Dmaven.compiler.showDeprecation=true"
 		CheckLastExitCode
 
 		if ($env:SQ_VERSION -eq "DEV")
